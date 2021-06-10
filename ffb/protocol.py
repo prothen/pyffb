2ea72c9e7aca751a214957d93b196176b8599e7d
Philipp Rothenhäusler <phirot@kth.se>
2021-06-10 23:54:45 +0200

    Update protocol definition

--- ffb/protocol.py
+++ ffb/protocol.py
@@ -2,61 +2,217 @@
 """
     Command, axis and message definitions.
 
+    For reference of the format string used for struct creation see
+
+    https://docs.python.org/3/library/struct.html
+
+    Basics:
+        float 'f'
+        signed / unsigned byte 'b' / 'B'
+        signed / unsigned int16 'h' / 'H'
+        signed / unsigned int32 'i' / 'I'
+
+
     Author: Philipp Rothenhäusler, Stockholm 2020
 
 """
 
 
 import enum
+import attr
+import numpy
+import typing
 
 
-class Command(enum.IntEnum):
-    DataRead = 0xD0
-    RemoteControlConfig = 0xD8
-    StateControl = 0xCD
-    SettingsControl = 0xCE
-    ProfileControl = 0xCF
-    IO2CANGatewayRead = 0xD2
-    IO2CANGatewayWrite = 0xD6
-    IO2CANGatewaySetTimeout = 0xD7
+# Response elements
+# Packetlength (uint16_t) |
+# (optional) Identifier (uint32_t) |
+# Status (uint8_t)
+# Response value (... see CommandType.format)
+RESPONSE_FORMAT = "HB"
+
+# In case of Messageidentifier uncomment below
+# RESPONSE_FORMAT = "HIB"
+
+
+@attr.s
+class CommandDefinition:
+    # Identifier from CLS2Sim protocol v2
+    identifier = attr.ib(type=int)
+    # Common format (implied additional arguments
+    request_format = attr.ib(type=str)
+    # Define request elements
+    request_labels = attr.ib(type=typing.List[str])
+    # Response format (excluding CommandType specifics to be appended)
+    response_format = attr.ib(type=str)
+    # Define response elements
+    response_labels = attr.ib(type=typing.List[str])
+
+    # Decide whether supplied value is encoded for request
+    sends_value = attr.ib(default=False, type=bool)
+    # Decide whether some received value is decoded from response
+    receives_value = attr.ib(default=False, type=bool)
+
+    # TODO: Possibly add different error style classes to handle varying cases
+
+
+class Command:
+
+    # Reading data: (command, axis, data_id)
+    StateControl = CommandDefinition(
+        identifier=0xCD,
+        request_format="III",
+        request_labels=["Command", "Axis",
+                        "StateCommand"],
+        response_format="HBHHH",
+        response_labels=["PacketLength", "Status",
+                         "DeviceId", "DeviceState", "DeviceTemperature"],
+        receives_value=True,)
+    # Reading data: (command, axis, data_id)
+    DataRead = CommandDefinition(
+        identifier=0xD0,
+        request_format="III",
+        request_labels=["Command", "Axis",
+                        "DataId"],
+        response_format="HBH",
+        response_labels=["PacketLength", "Status",
+                         "DeviceId", "Data"],
+        receives_value=True,)
+
+    # TODO: Type specification
+    # RemoteControlConfig = CommandDefinition(
+    #     identifier=0xD8)
+
+    # TODO: Type specification
+    # StateControl = CommandDefinition(
+    #     identifier=0xCD)
+
+    # TODO: Type specification
+    SettingsControl = CommandDefinition(
+        identifier=0xCE,
+        request_format="iii",
+        request_labels=["Command", "Axis",
+                        "SettingId", "SettingValue"],
+        response_format="HB",
+        response_labels=["PacketLength", "Status"],
+        sends_value=True)
+
+    # ProfileControl = CommandDefinition(
+    #     identifier=0xCF)
+    # IO2CANGatewayRead = CommandDefinition(
+    #     identifier=0xD2)
+    # IO2CANGatewayWrite = CommandDefinition(
+    #     identifier=0xD6)
+    # IO2CANGatewaySetTimeout = CommandDefinition(
+    #     identifier=0xD7)
+
+    #
+    OverrideControl = CommandDefinition(
+        identifier=0xD1,
+        request_format="III",
+        request_labels=["Command", "Axis",
+                        "OverrideId"],
+        response_format="HB",
+        response_labels=["PacketLength", "Status"],)
 
 
 class Axis(enum.IntEnum):
-    X = 0
-    Y = 1
+    X = 0x01
+    Y = 0x02
 
 
-class Measurement(enum.IntEnum):
-    Position = 0
-    PositionNormalized = 1
-    AppliedForce = 2
-    AppliedForceNormalized = 3
-    DigitalInputs = 4
-    AnalogInputs = 5
-    VirtualForce = 6
-    Range = 7
+@attr.s
+class ValueConstraints:
+    """ Scalar value constraint convenience object. """
+    # TODO: Auto convert any type to numpy.ndarray
+    min = attr.ib(default=-numpy.inf) #, type=numpy.ndarray)
+    max = attr.ib(default=numpy.inf) # , type=numpy.ndarray)
+    ##
+    # is_type = attr.ib(default=None)
+
+    def is_satisfied(self, value: numpy.ndarray):
+        # Check type instance
+        return self.min <= value <= self.max
 
 
-# Note: Other measurements are added only on demand
-
-measurement2format = dict()
-measurement2format[Measurement.Position] = "h"
-measurement2format[Measurement.PositionNormalized] = "f"
-measurement2format[Measurement.AppliedForce] = "i"
-measurement2format[Measurement.AppliedForceNormalized] = "f"
-measurement2format[Measurement.Range] = "iiii"
-
-
-axis2axis_id = dict()
-axis2axis_id[Axis.X] = 0x01
-axis2axis_id[Axis.Y] = 0x02
+@attr.s
+class CommandType:
+    # Define identifier for command interface
+    identifier = attr.ib(type=int)
+    # Define command type specific return or send format
+    format = attr.ib(default=None, type=typing.Optional[str])
+    # Value range and type specifications
+    value_range = attr.ib(default=ValueConstraints(), type=ValueConstraints)
 
 
-measurement2data_id = dict()
-measurement2data_id[Measurement.Position] = 0x10
-measurement2data_id[Measurement.PositionNormalized] = 0x11
-measurement2data_id[Measurement.AppliedForce] = 0x20
-measurement2data_id[Measurement.AppliedForceNormalized] = 0x21
-measurement2data_id[Measurement.Range] = 0x60
+class DataRead:
+    Position = CommandType(format="h", identifier=0x10)
+    PositionNormalized = CommandType(format="f", identifier=0x11)
+    AppliedForce = CommandType(format="i", identifier=0x20)
+    AppliedForceNormalized = CommandType(format="f", identifier=0x21)
+    # DigitalInputs = CommandEntry(format="h", identifier=0x10)
+    # AnalogInputs = CommandEntry(format="h", identifier=0x10)
+    # VirtualForce = CommandEntry(format="h", identifier=0x10)
+    Range = CommandType(format="iiii", identifier=0x60)
 
 
+class SettingsControl:
+    ForceProfile = CommandType(
+        format="H", identifier=0x10,
+        value_range=ValueConstraints(min=5, max=99999))
+    ForceScaleFactor = CommandType(
+        format="H", identifier=0x20,
+        value_range=ValueConstraints(min=20, max=999))
+    FrictionValue = CommandType(
+        format="H", identifier=0x25,
+        value_range=ValueConstraints(min=20, max=65535))
+    FrictionValueEng = CommandType(
+        format="f", identifier=0x26,
+        value_range=ValueConstraints(min=.1))
+
+    PositionWindow = CommandType(
+        format="I", identifier=0x27,
+        value_range=ValueConstraints(min=0, max=2**32))
+    TargetTorque = CommandType(
+        format="h", identifier=0x28,
+        value_range=ValueConstraints(min=0, max=65535))
+    ProfileMode = CommandType(
+        format="B", identifier=0x30,
+        value_range=ValueConstraints(min=0, max=1))
+    MoveBackEnable = CommandType(
+        format="B", identifier=0x40,
+        value_range=ValueConstraints(min=0, max=1))
+
+    AutopilotForce = CommandType(
+        format="H", identifier=0x50,
+        value_range=ValueConstraints(min=5, max=1000))
+    AutopilotSpeed = CommandType(
+        format="H", identifier=0x60,
+        value_range=ValueConstraints(min=5, max=10))
+    AutopilotEnable = CommandType(
+        format="B", identifier=0x70,
+        value_range=ValueConstraints(min=0, max=1))
+    AutopilotPosition = CommandType(
+        format="f", identifier=0x80,
+        value_range=ValueConstraints(min=-1, max=1))
+
+    VibrationActive = CommandType(
+        format="B", identifier=0xC0,
+        value_range=ValueConstraints(min=0, max=1))
+    VibrationSpeedForFastVibrations = CommandType(
+        format="I", identifier=0xC1,
+        value_range=ValueConstraints(min=10))
+
+
+class OverrideControl:
+    """
+
+        Note:
+                To disable all override send 0x00 as axis.
+    """
+
+    ScaleFactorOverride = CommandType(identifier=0x01)
+    TrimPositionOverride = CommandType(identifier=0x02)
+    AutopilotEnableOverride = CommandType(identifier=0x03)
+    AutopilotPositionOverride = CommandType(identifier=0x04)
+    VibrationOverride = CommandType(identifier=0x05)
