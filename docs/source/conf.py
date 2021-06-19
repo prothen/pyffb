94b1a7e8244567f43d91ddb34ad5771765127224
Philipp Rothenhäusler <phirot@kth.se>
2021-06-19 06:41:16 +0200

    Update documentation with source reference

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,7 +32,8 @@ release = '1.0.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-        'sphinx.ext.autodoc'
+        'sphinx.ext.autodoc',
+        'sphinx.ext.viewcode'
 ]
 extensions.append("guzzle_sphinx_theme")
 
